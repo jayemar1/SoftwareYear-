ac7fcd14aa02758c1381f3b4b10ee2a7c56fd949
jayemar1 <51124661+jayemar1@users.noreply.github.com>
2020-11-06 15:15:28 +0000

    Add files via upload
    Final code that is full commented, explain different parts of the code.

--- Software project/James Meek Rose - csv parse and data visualisation.py
+++ Software project/James Meek Rose - csv parse and data visualisation.py
@@ -6,12 +6,13 @@ Created on Thu Nov  5 13:00:17 2020
 """
 
 #Imports all the modules that allows me to mangage the data how I would like using their unique properties
-#'plt' and 'np' names are given to their respective module for ease of use
+#'plt', 'pd' and 'np' names are given to their respective module for ease of use
 
 import csv
-import numpy as np
+import pandas as pd
 from collections import Counter
 import matplotlib.pyplot as plt
+
 
 
 #Calls on the selected file
@@ -21,23 +22,27 @@ with open('data1.csv') as data:
     #A variable that stores the 'reader' method with csv file instructions as an argument
     readFile = csv.DictReader(data)
     
-   
+    #Variable for the counter function
     speedCount = Counter()
     
+    #loops through all data in csv file
+    #updates the counter with the data for speed
     for row in readFile:
         speedCount.update(row['00'].split(','))
-        
+
+#Arrays for the data to be stored        
 speed = []
 carsTravellingAtThatSpeed = []
 
+#Only displays the 10 most common recorded speeds using the 'most_common' method
+#Joins items to their relevant variables
 for item in speedCount.most_common(10):
     speed.append(item[0])
     carsTravellingAtThatSpeed.append(item[1]) 
-    
+
+
+#Creates bar graph and plots data on x and y axis   
 plt.bar(speed, carsTravellingAtThatSpeed)
-
-
-
 
 #Add title to graph
 plt.title('Top 10 speeds recorded by 200 cars')
@@ -51,10 +56,51 @@ plt.ylabel('Number of cars travelling at X speed')
 #Adds a black background to the graph
 plt.style.use('dark_background')
 
+#Adds grid to graph
 plt.grid()
 
 #Displays the graph
 plt.show()
 
+#Analysis of data
 
-    
+#Variable that uses pandas to read the csv file
+calc = pd.read_csv ('data1.csv')
+
+#Calculates average
+average = calc['00'].mean()
+print("The avarage speed is: ",average)
+
+#Calculates median
+showMedian = calc['00'].median() 
+print("The median speed is: ",showMedian)
+
+#Calculates standard deviation 
+sd = calc['00'].std() 
+print("The standard deviation of the speed is: ",sd)
+
+#Calculates sum 
+showSum = calc['00'].sum() 
+print("The sum of the speed is: ",showSum)
+
+#Calculates variation 
+showsVariation = calc['00'].var() 
+print("The variation of the speed is: ",showsVariation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
